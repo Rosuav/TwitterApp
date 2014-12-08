e7974d674eeb511f1058bdf9806535b064fba2cc
vishwanath79@outlook.com <vishwanath79@outlook.com>
2014-12-07 17:02:18 -0800

    Source the Twitter API

--- tweetful.py
+++ tweetful.py
@@ -1,6 +1,11 @@
+import authorization
+import json
+import requests
+from urls import *
 def main():
-	""" Main functiion """
-	pass
+	auth = authorization.authorize()
+	response = requests.get(TIMELINE_URL,auth=auth)
+	print json.dumps(response.json(), indent =4)
 	
 if __name__ == "__main__":
 	main()
