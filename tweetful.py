05f566a6b41aadfd0c245ff29754e0778a8aad39
vishwanath79@outlook.com <vishwanath79@outlook.com>
2014-12-16 20:22:13 -0800

    Adding arg parse

--- tweetful.py
+++ tweetful.py
@@ -6,11 +6,9 @@ import argparse
 import gettwitter
 from urls import *
 def main():
-	
 	parser = argparse.ArgumentParser(description='Interact with twitterAPI')
-	parser.add_argument('-twitter', help='Add this switch to show your retweets. Options are status,myretweets,followers,favorites,hometimeline')
+	parser.add_argument('-twitter', help='Add this switch to show your retweets. Options are status,myretweets,followers,favorites,hometimeline', nargs='?')
 	args = parser.parse_args()
-		
 	for arg in sys.argv:
 		if sys.argv[2] == "status":
 			arg = "/1.1/statuses/lookup.json"
@@ -23,14 +21,18 @@ def main():
 		elif sys.argv[2] == "hometimeline":
 			arg = "/1.1/statuses/home_timeline.json"
 		elif sys.argv[2] == "favorites":
-			arg = "/1.1/favorites/list.json"
-		
+				arg = "/1.1/favorites/list.json"
 	
-	ARG_URL = API_URL + arg 
+		ARG_URL = API_URL + arg
 	print ARG_URL 
 	auth = authorization.authorize()
 	response = requests.get(ARG_URL,auth=auth)
 	print json.dumps(response.json(), indent =4)
 
 if __name__ == "__main__":
-	main()
+	try: 
+		main()
+		
+	except Exception:
+		print 'Type -h for help on arguments to pass'
+	
